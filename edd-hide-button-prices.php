4bf5be36ee53d0f3ad7587d5520cb0344bd68618
Ghost1227 <dgriffiths@ghost1227.com>
2014-02-21 03:01:06 +0200

    Convert tabs to spaces

--- edd-hide-button-prices.php
+++ edd-hide-button-prices.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name:		Easy Digital Downloads - Hide Button Prices
- * Plugin URI:		http://wordpress.org/plugins/easy-digital-downloads-hide-button-prices/
- * Description:		Removes prices from purchase buttons on Easy Digital Downloads
- * Version:			1.0.1
- * Author:			Daniel J Griffiths
- * Author URI:		http://section214.com
- * Text Domain:		edd-hide-button-prices
+ * Plugin Name:     Easy Digital Downloads - Hide Button Prices
+ * Plugin URI:      http://wordpress.org/plugins/easy-digital-downloads-hide-button-prices/
+ * Description:     Removes prices from purchase buttons on Easy Digital Downloads
+ * Version:         1.0.1
+ * Author:          Daniel J Griffiths
+ * Author URI:      http://section214.com
+ * Text Domain:     edd-hide-button-prices
  * 
- * @package			EDD\HideButtonPrices
- * @author			Daniel J Griffiths <dgriffiths2section214.com>
+ * @package         EDD\HideButtonPrices
+ * @author          Daniel J Griffiths <dgriffiths2section214.com>
  */
 
 // Exit if accessed directly
@@ -19,74 +19,74 @@ if( !defined( 'ABSPATH' ) ) exit;
 if( !class_exists( 'EDD_Hide_Button_Prices' ) ) {
 
 
-	/**
-	 * Main EDD_Hide_Button_Prices class
-	 *
-	 * @since		1.0.1
-	 */
-	class EDD_Hide_Button_Prices {
+    /**
+     * Main EDD_Hide_Button_Prices class
+     *
+     * @since       1.0.1
+     */
+    class EDD_Hide_Button_Prices {
 
 
-		/**
-		 * @var			EDD_Hide_Button_Prices $instance The one true EDD_Hide_Button_Prices
-		 * @since		1.0.1
-		 */
-		private static $instance;
+        /**
+         * @var         EDD_Hide_Button_Prices $instance The one true EDD_Hide_Button_Prices
+         * @since       1.0.1
+         */
+        private static $instance;
 
 
-		/**
-		 * Get active instance
-		 *
-		 * @access		public
-		 * @since		1.0.0
-		 * @return		object self::$instance The one true EDD_Hide_Button_Prices
-		 */
-		public static function instance() {
-			if( !self::$instance ) {
-				self::$instance = new EDD_Hide_Button_Prices();
-				self::$instance->load_textdomain();
-				self::$instance->hooks();
-			}
+        /**
+         * Get active instance
+         *
+         * @access      public
+         * @since       1.0.0
+         * @return      object self::$instance The one true EDD_Hide_Button_Prices
+         */
+        public static function instance() {
+            if( !self::$instance ) {
+                self::$instance = new EDD_Hide_Button_Prices();
+                self::$instance->load_textdomain();
+                self::$instance->hooks();
+            }
 
-			return self::$instance;
-		}
-
-
-		/**
-		 * Run action and filter hooks
-		 *
-		 * @access		private
-		 * @since		1.0.0
-		 * @return		void
-		 */
-		private function hooks() {
-			// Edit plugin metalinks
-			add_filter( 'plugin_row_meta', array( $this, 'plugin_metalinks' ), null, 2 );
-
-			// Override prices
-			add_filter( 'edd_purchase_link_defaults', array( $this, 'hide_button_prices' ) );
-		}
+            return self::$instance;
+        }
 
 
-		/**
-		 * Internationalization
-		 *
-		 * @access		public
-		 * @since		1.0.0
-		 * @return		void
-		 */
-		public function load_textdomain() {
-			// Set filter for language directory
-			$lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
-			$lang_dir = apply_filters( 'EDD_Hide_Button_Prices_lang_dir', $lang_dir );
+        /**
+         * Run action and filter hooks
+         *
+         * @access      private
+         * @since       1.0.0
+         * @return      void
+         */
+        private function hooks() {
+            // Edit plugin metalinks
+            add_filter( 'plugin_row_meta', array( $this, 'plugin_metalinks' ), null, 2 );
 
-			// Traditional WordPress plugin locale filter
-			$locale		= apply_filters( 'plugin_locale', get_locale(), '' );
-			$mofile		= sprintf( '%1$s-%2$s.mo', 'edd-hide-button-prices', $locale );
+            // Override prices
+            add_filter( 'edd_purchase_link_defaults', array( $this, 'hide_button_prices' ) );
+        }
 
-			// Setup paths to current locale file
-			$mofile_local	= $lang_dir . $mofile;
-			$mofile_global	= WP_LANG_DIR . '/edd-hide-button-prices/' . $mofile;
+
+        /**
+         * Internationalization
+         *
+         * @access      public
+         * @since       1.0.0
+         * @return      void
+         */
+        public function load_textdomain() {
+            // Set filter for language directory
+            $lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
+            $lang_dir = apply_filters( 'EDD_Hide_Button_Prices_lang_dir', $lang_dir );
+
+            // Traditional WordPress plugin locale filter
+            $locale     = apply_filters( 'plugin_locale', get_locale(), '' );
+            $mofile     = sprintf( '%1$s-%2$s.mo', 'edd-hide-button-prices', $locale );
+
+            // Setup paths to current locale file
+            $mofile_local   = $lang_dir . $mofile;
+            $mofile_global  = WP_LANG_DIR . '/edd-hide-button-prices/' . $mofile;
 
             if( file_exists( $mofile_global ) ) {
                 // Look in global /wp-content/languages/edd-hide-button-prices/ folder
@@ -98,7 +98,7 @@ if( !class_exists( 'EDD_Hide_Button_Prices' ) ) {
                 // Load the default language files
                 load_plugin_textdomain( 'edd-hide-button-prices', false, $lang_dir );
             }
-		}
+        }
 
 
         /**
@@ -124,23 +124,23 @@ if( !class_exists( 'EDD_Hide_Button_Prices' ) ) {
             }
 
             return $links;
-		}
+        }
 
 
-		/**
-		 * Hide button prices
-		 *
-		 * @access			public
-		 * @since			1.0.1
-		 * @param			array $defaults
-		 * @return			array
-		 */
-		public function hide_button_prices( $defaults ) {
-			$defaults['price'] = (bool) false;
+        /**
+         * Hide button prices
+         *
+         * @access          public
+         * @since           1.0.1
+         * @param           array $defaults
+         * @return          array
+         */
+        public function hide_button_prices( $defaults ) {
+            $defaults['price'] = (bool) false;
 
-			return $defaults;
-		}
-	}
+            return $defaults;
+        }
+    }
 }
 
 
@@ -148,19 +148,19 @@ if( !class_exists( 'EDD_Hide_Button_Prices' ) ) {
  * The main function responsible for returning the one true EDD_Hide_Button_Prices
  * instance to functions everywhere
  *
- * @since		1.0.0
- * @return		EDD_Hide_Button_Prices The one true EDD_Hide_Button_Prices
+ * @since       1.0.0
+ * @return      EDD_Hide_Button_Prices The one true EDD_Hide_Button_Prices
  */
 function EDD_Hide_Button_Prices_load() {
-	if( !class_exists( 'Easy_Digital_Downloads' ) ) {
-		deactivate_plugins( __FILE__ );
-		unset( $_GET['activate'] );
+    if( !class_exists( 'Easy_Digital_Downloads' ) ) {
+        deactivate_plugins( __FILE__ );
+        unset( $_GET['activate'] );
 
-		// Display notice
-		add_action( 'admin_notices', 'EDD_Hide_Button_Prices_missing_edd_notice' );
-	} else {
-		return EDD_Hide_Button_Prices::instance();
-	}
+        // Display notice
+        add_action( 'admin_notices', 'EDD_Hide_Button_Prices_missing_edd_notice' );
+    } else {
+        return EDD_Hide_Button_Prices::instance();
+    }
 }
 add_action( 'plugins_loaded', 'EDD_Hide_Button_Prices_load' );
 
@@ -168,9 +168,9 @@ add_action( 'plugins_loaded', 'EDD_Hide_Button_Prices_load' );
 /**
  * We need Easy Digital Downloads... if it isn't present, notify the user!
  *
- * @since		1.1.0
- * @return		void
+ * @since       1.1.0
+ * @return      void
  */
 function EDD_Hide_Button_Prices_missing_edd_notice() {
-	echo '<div class="error"><p>' . __( 'Hide Button Prices requires Easy Digital Downloads! Please install it to continue!', 'edd-balanced-gateway' ) . '</p></div>';
+    echo '<div class="error"><p>' . __( 'Hide Button Prices requires Easy Digital Downloads! Please install it to continue!', 'edd-balanced-gateway' ) . '</p></div>';
 }
